d8ca5231de8085622244e536144b2f302b75e88e
danersound <andre.barreto28@gmail.com>
2024-09-26 23:26:02 +0300

    added two more functions

--- index.js
+++ index.js
@@ -33,11 +33,36 @@ app.post('/api/addTraceurs', async (req,res)=>{
     }
 });
 
-app.get('/api/Traceurs'), async (req,res) => {
+app.get('/api/Traceurs', async (req, res) => {
     try {
-        const Students = await Traceur.find({})
-        res.status(200).json(Students)
-    }catch(error){
-        res.status(500).json({message:error.message})
+        const Students = await Traceur.find({});
+        res.status(200).json(Students);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-}
+});
+
+app.get('/api/Traceur/:id',  async (req, res)=> {
+    try {
+        const {id} = req.params;
+        const Student = await Traceur.findById(id);
+        res.status(200).json(Student);
+    }catch (error){
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// update a data student
+app.put('/api/product/:id', async (req, res) => {
+   try{
+        const {id} = req.params;
+        const Student = await Traceur.findByIdAndUpdate(id, req.body)
+        if(!Student){
+            res.status(404).json({ message: " Traceur non trovato!" });
+        }
+        const StudentUpdated = await Traceur.findById(id, req.body)
+        res.status(200).json(StudentUpdated);
+   }catch(error){
+    es.status(500).json({ message: error.message });
+   } 
+});
